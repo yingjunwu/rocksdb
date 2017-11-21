f8615e5578a9a5e73bb4cfda775270dafede35cc
Yingjun Wu <yingjun.wu@ibm.com>
2017-11-20 17:32:04 -0800

    update benchmark

--- examples/ycsb_benchmark.cc
+++ examples/ycsb_benchmark.cc
@@ -3,8 +3,6 @@
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
 
-#ifndef ROCKSDB_LITE
-
 #include "rocksdb/db.h"
 #include "rocksdb/options.h"
 #include "rocksdb/slice.h"
@@ -20,6 +18,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <thread>
 
 using namespace rocksdb;
 
@@ -27,17 +26,17 @@ struct YcsbConfig {
 
   const size_t default_table_size_ = 1000;
   
-  size_t thread_count_;
+  size_t thread_count_; // number of threads
   
-  float scale_factor_;
+  float scale_factor_; // scale factor
   
-  float zipf_theta_;
+  float zipf_theta_; // skewness
   
-  // operation count in a transaction
-  size_t operation_count_;
+  size_t operation_count_; // number of operations in a transaction
 
-  // update ratio
-  float update_ratio_;
+  float update_ratio_; // update ratio
+
+  float duration_; // time duration (s)
 
 };
 
@@ -50,6 +49,7 @@ void Usage(FILE *out) {
           "   -z --zipf_theta        :  zipf theta \n"
           "   -o --operation_count   :  # of operations \n"
           "   -u --update_ratio      :  update ratio \n"
+          "   -d --duration          :  total processing time duration \n"
   );
 }
 
@@ -59,6 +59,7 @@ static struct option opts[] = {
     { "zipf_theta", optional_argument, NULL, 'z' },
     { "operation_count", optional_argument, NULL, 'o' },
     { "update_ratio", optional_argument, NULL, 'u' },
+    { "duration", optional_argument, NULL, 'd' },
     { NULL, 0, NULL, 0 }
 };
 
@@ -69,11 +70,12 @@ void ParseArguments(int argc, char *argv[], YcsbConfig &conf) {
   conf.zipf_theta_ = 0;
   conf.operation_count_ = 1;
   conf.update_ratio_ = 0;
+  conf.duration_ = 10;
   
   // parse args
   while (1) {
     int idx = 0;
-    int c = getopt_long(argc, argv, "ht:k:z:o:u:", opts, &idx);
+    int c = getopt_long(argc, argv, "ht:k:z:o:u:d:", opts, &idx);
 
     if (c == -1) break;
 
@@ -92,6 +94,9 @@ void ParseArguments(int argc, char *argv[], YcsbConfig &conf) {
         break;
       case 'u':
         conf.update_ratio_ = atof(optarg);
+        break;
+      case 'd':
+        conf.duration_ = atof(optarg);
         break;
       case 'h':
       Usage(stderr);
@@ -112,7 +117,8 @@ void Populate(TransactionDB* txn_db, const YcsbConfig &config) {
 
   size_t table_size = config.default_table_size_ * config.scale_factor_;
   
-  std::cout << ">>>>> Populate table. " << std::endl
+  std::cout << std::endl
+            << ">>>>> Populate table. " << std::endl
             << "   -- Table size   : " << table_size << std::endl;
 
   WriteOptions write_options;
@@ -136,9 +142,12 @@ void Populate(TransactionDB* txn_db, const YcsbConfig &config) {
   txn = nullptr;
 }
 
-void ProcessTransaction(TransactionDB* txn_db, const YcsbConfig &config) {
+volatile bool is_running = true;
+size_t *commit_counts;
+
+void ProcessThread(TransactionDB* txn_db, const YcsbConfig &config, const size_t thread_id) {
   
-  srand(time(NULL));
+  size_t &commit_count_ref = commit_counts[thread_id];
 
   size_t table_size = config.default_table_size_ * config.scale_factor_;
 
@@ -150,41 +159,90 @@ void ProcessTransaction(TransactionDB* txn_db, const YcsbConfig &config) {
   ReadOptions read_options;
   TransactionOptions txn_options;
   std::string value;
+  
+  while (true) {
+    if (is_running == false) {
+      break;
+    }
+    // Start a transaction
+    Transaction* txn = txn_db->BeginTransaction(write_options);
+    assert(txn);
 
-  std::cout << ">>>>> Process transactions. " << std::endl
+    Status s;
+
+    for (size_t i = 0; i < config.operation_count_; ++i) {
+      
+      size_t key = zipf.GetNextNumber() - 1;
+      
+      if (fast_rand.next_uniform() < config.update_ratio_) {
+        // update
+        s = txn->Put(std::to_string(key), "z");
+        assert(s.ok());
+      } else {
+        // select
+        s = txn_db->Get(read_options, std::to_string(key), &value);
+        // std::cout << "value = " << value << std::endl;
+        assert(s.ok());
+      }
+    }
+
+    // Commit transaction
+    s = txn->Commit();
+    assert(s.ok());
+    delete txn;
+    txn = nullptr;
+    commit_count_ref++;
+  }
+
+}
+
+void Process(TransactionDB* txn_db, const YcsbConfig &config) {
+  
+  size_t table_size = config.default_table_size_ * config.scale_factor_;
+
+  std::cout << std::endl
+            << ">>>>> Process transactions. " << std::endl
             << "   -- Table size               : " << table_size << std::endl
             << "   -- Operation count          : " << config.operation_count_ << std::endl
             << "   -- Update ratio             : " << config.update_ratio_ << std::endl
-            << "   -- Zipf theta               : " << config.zipf_theta_ << std::endl;
+            << "   -- Zipf theta               : " << config.zipf_theta_ << std::endl
+            << "   -- Duration                 : " << config.duration_ << std::endl;
+
+
+  size_t thread_count = config.thread_count_;
+
+  commit_counts = new size_t[thread_count];
+  memset(commit_counts, 0, sizeof(size_t) * thread_count);
+
+  std::vector<std::thread> thread_group;
+
+  for (size_t i = 0; i < thread_count; ++i) {
+    thread_group.push_back(
+      std::move(std::thread(ProcessThread, txn_db, std::ref(config), i)));
+  }
   
+  std::this_thread::sleep_for(
+      std::chrono::milliseconds(int(config.duration_ * 1000)));
 
-  // Start a transaction
-  Transaction* txn = txn_db->BeginTransaction(write_options);
-  assert(txn);
+  is_running = false;
 
-  Status s;
-
-  for (size_t i = 0; i < config.operation_count_; ++i) {
-    
-    size_t key = zipf.GetNextNumber() - 1;
-    
-    if (fast_rand.next_uniform() < config.update_ratio_) {
-      // update
-      s = txn->Put(std::to_string(key), "z");
-      assert(s.ok());
-    } else {
-      // select
-      s = txn_db->Get(read_options, std::to_string(key), &value);
-      std::cout << "value = " << value << std::endl;
-      assert(s.ok());      
-    }
+  for (size_t i = 0; i < thread_count; ++i) {
+    thread_group[i].join();
   }
 
-  // Commit transaction
-  s = txn->Commit();
-  assert(s.ok());
-  delete txn;
-  txn = nullptr;
+  size_t total_commit_count = 0;
+  for (size_t i = 0; i < thread_count; ++i) {
+    total_commit_count += commit_counts[i];
+  }
+
+  float tps = total_commit_count / config.duration_ / 1000;
+  std::cout << std::endl 
+            << ">>>>> Results. " << std::endl
+            << "   -- TPS             : " << tps << " K txn/s" << std::endl
+            << "   -- Per-thread TPS  : " << tps / config.thread_count_ << " K txn/s" << std::endl;
+
+  delete[] commit_counts;
+  commit_counts = nullptr;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -193,6 +251,8 @@ void ProcessTransaction(TransactionDB* txn_db, const YcsbConfig &config) {
 std::string kDBPath = "/tmp/rocksdb_transaction_example";
 
 int main(int argc, char **argv) {
+
+  srand(time(NULL));
   
   YcsbConfig config;
 
@@ -211,12 +271,13 @@ int main(int argc, char **argv) {
 
   Populate(txn_db, config);
 
-  ProcessTransaction(txn_db, config);
+  Process(txn_db, config);
 
   // Cleanup
   delete txn_db;
   DestroyDB(kDBPath, options);
+
+  std::cout << std::endl;
+
   return 0;
 }
-
-#endif  // ROCKSDB_LITE
